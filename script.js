f64c5a78ad6c8c680c0535ad34ed7d001c6683f6
ChaseWelsh <fseusb@gmail.com>
2022-07-26 19:41:40 -0600

    Refine about, pointcloud edgewrap, clean code

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
+const width = window.innerWidth;
+const height = window.innerHeight;
+const screenMaxLength = Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2));
+
+// PointCloud Canvas Definition
 const canvas = document.getElementById('background');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
+canvas.width = width
+canvas.height = height
 const ctx = canvas.getContext('2d');
-
-ctx.strokeStyle = "#FFFFFF";
 ctx.fillStyle = "#FFFFFF";
 
+// PointCloud Settings
+numpoints = 30;
+maxspeed = 0.25;
+
+// Math Functions
 function rand(min, max) {
     return Math.random() * (max - min) + min;
 }
@@ -17,18 +24,22 @@ function dist(point0, point1) {
     return Math.sqrt(Math.pow(point0.x - point1.x, 2) + Math.pow(point0.y - point1.y, 2));
 }
 
+// Drawing functions
 function drawPoint(point) {
     ctx.beginPath();
     ctx.arc(point.x, point.y, point.size, 0, 360);
     ctx.fill();
 }   
-
-const screenMaxLength = Math.sqrt(Math.pow(window.innerWidth, 2) + Math.pow(window.innerHeight, 2));
 function drawLine(point0, point1) {
     distance = dist(point0, point1);
     transparency = ((1-(distance / screenMaxLength))-0.75)*5;
     if (transparency > 0) {
+        // lines can get thicker than 2 if points are really close
         ctx.lineWidth = transparency*2;
+        // but make sure transparency never goes above 100% because that causes issues
+        if (transparency>=1){
+            transparency=1;
+        }
         ctx.strokeStyle = `#FFFFFF${(parseInt(transparency*255)).toString(16)}`;
 
         ctx.beginPath();
@@ -38,14 +49,13 @@ function drawLine(point0, point1) {
     }
 }
 
-numpoints = 25;
-maxspeed = 0.25;
+
 points = [];
+// Initalize points with random positions, velocities, and sizes
 for (let i = 0; i < numpoints; i++) {
-    // x, y, xV, yV, size
     points.push( {
-        x: rand(0, window.innerWidth), 
-        y: rand(0, window.innerHeight), 
+        x: rand(0, width), 
+        y: rand(0, height), 
         xV: rand(-maxspeed, maxspeed), 
         yV: rand(-maxspeed, maxspeed), 
         size: rand(3, 5)
@@ -54,22 +64,32 @@ for (let i = 0; i < numpoints; i++) {
 
 function renderFrame() {
     // clear canvas for next frame
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, height);
 
     // iterate through all the points, move them one step, draw them
     for (let i = 0; i < numpoints; i++) {
         // apply velocity 1 step and save new position
         point = points[i];
-        point.x = mod((point.x + point.xV), window.innerWidth);
-        point.y = mod((point.y + point.yV), window.innerHeight);
+        point.x = mod((point.x + point.xV), width);
+        point.y = mod((point.y + point.yV), height);
         points[i] = point;
 
         // draw point at new position
         drawPoint(point);
 
         // draw lines between all points 
-        for (let c = i+1; c < numpoints; c++) {
-            drawLine(point, points[c]);
+        for (let c = 0; c < numpoints; c++) {
+            drawLine(point, {x:-(width-points[c].x), y: -(height-points[c].y)})
+            drawLine(point, {x:-(width-points[c].x), y: points[c].y})
+            drawLine(point, {x:-(width-points[c].x), y: height+points[c].y})
+
+            drawLine(point, {x:points[c].x, y: -(height-points[c].y)})
+            if (c > i) { drawLine(point, points[c]); }
+            drawLine(point, {x:points[c].x, y: height+points[c].y})
+
+            drawLine(point, {x:width+points[c].x, y: -(height-points[c].y)})
+            drawLine(point, {x:width+points[c].x, y: points[c].y});
+            drawLine(point, {x:width+points[c].x, y: height+points[c].y})
         }
     }
 }
