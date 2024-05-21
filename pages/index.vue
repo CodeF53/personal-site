1d8f333d58c61ad6042d2c55db41ce9f7ba97acc
CodeF53 <fseusb@gmail.com>
2024-05-21 13:16:42 -0600

    remove marquee

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,7 @@ definePageMeta({ layout: 'home' })
     />
 
     <main class="col gap2">
-      <div class="marquee"><h1>Hi, I'm F53</h1></div>
+      <h1>Hi, I'm F53</h1>
 
       <p class="bio">
         I'm a {{ Math.floor((Date.now() / 100000 - 10463292) / 315360) }}yo Full Stack Developer
@@ -55,20 +55,5 @@ definePageMeta({ layout: 'home' })
   }
 
   h1 { color: var(--accent-light); }
-  
-  .marquee {
-    white-space: nowrap;
-    overflow: hidden;
-    box-sizing: border-box;
-  }
-  .marquee * {
-    display: inline-block;
-    padding-left: 100%;
-    animation: marquee 15s linear infinite;
-  }
-  @keyframes marquee {
-    0%   { transform: translate(0, 0); }
-    100% { transform: translate(-100%, 0); }
-  }
 }
 </style>
