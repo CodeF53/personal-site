afde8c2c4bbe983afb22f25731ee7af80def0f5e
cassie <fseusb@gmail.com>
2024-11-21 11:16:16 -0700

    allow people without websites into friends so I don't feel awful about removing associations with vencordians

--- pages/friends.vue
+++ pages/friends.vue
@@ -1,18 +1,16 @@
 <script lang="ts" setup>
 // id is discord UserID
 const friends = ref([
-  { id: '397230646231040002', url: 'https://shoritsu.xyz/' },
-  { id: '1012800881474949221', url: 'https://viggy.moe/' },
-  { id: '545581357812678656', url: 'https://megu.dev' },
-  { id: '923313823195205645', url: 'https://www.deatg.com/' },
-  { id: '381494697073573899', url: 'https://elissa.moe/' },
-  { id: '343383572805058560', url: 'https://vendicated.dev/' },
-  { id: '597158211459612675', url: 'https://dehtrashpanda.neocities.org/' },
-  { id: '1215758862444007465', url: 'https://tiramisu.gay' },
-  { id: '516750892372852754', url: 'https://aspy.dev' },
-  { id: '734079736615141426', url: 'https://catboys.zip/' },
-  { id: '1262793452236570667', url: 'https://cute.fm' },
-  { id: '156320970448175104', url: 'https://git.io/sr229' },
+  { id: '923313823195205645', url: 'www.deatg.com/' },
+  { id: '1262793452236570667', url: 'cute.fm' },
+  { id: '156320970448175104', url: 'git.io/sr229' },
+  { id: '683830555674279994', url: 'github.com/atvaster' },
+  { id: '1043599230247374869', url: 'github.com/mochienya' },
+  { id: '331576593342529537', url: 'github.com/Obscure2020' },
+  { id: '721351466828300350', url: 'github.com/Ran-Mewo' },
+  { id: '131802833199693825', url: 'greyw0lv.github.io' },
+  { id: '176384198570016768', url: 'github.com/mistrk7' },
+  { id: '909088839769944114', url: 'github.com/rhysdh540' },
 ])
 onMounted(() => {
   let currentIndex = friends.value.length
@@ -31,7 +29,7 @@ const avatarProxy = 'https://dp.nea.moe/avatar/'
     <h1>Friends</h1>
     <ul class="friend_list wrap centerChildren">
       <li v-for="friend in friends" :key="friend.id" class="interactive">
-        <NuxtLink :to="friend.url">
+        <NuxtLink :to="`https://${friend.url}`">
           <img :src="`${avatarProxy}${friend.id}.png`">
         </NuxtLink>
       </li>
